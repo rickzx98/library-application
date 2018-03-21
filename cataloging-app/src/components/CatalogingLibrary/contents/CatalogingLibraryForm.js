903b0338bd2321bc36e79e8de29133d6e1698d82
Jerico <rickzx98@gmail.com>
2018-03-22 02:07:26 +0800

    updates

--- cataloging-app/src/components/CatalogingLibrary/contents/CatalogingLibraryForm.js
+++ cataloging-app/src/components/CatalogingLibrary/contents/CatalogingLibraryForm.js
@@ -6,19 +6,24 @@ import PropTypes from 'prop-types';
 import React from 'react';
 export const CatalogingLibraryForm = ({ catalogingLibrary }) => {
   return (<CatalogingLibraryPageBody>
-    <FluidForm name="catalogingLibrary" specs={CatalogingLibrary}>
-      {catalogingLibrary && catalogingLibrary.data && Object.keys(catalogingLibrary.data).map((field, index)=> {
-        return (<FormGroup label={FluidForm.getLabel(catalogingLibrary, field)} name={field}
-                           className={`${ index === 1 ? 'col-sm-12' :
+    <div className="col-md-7">
+      <FluidForm name="catalogingLibrary" specs={CatalogingLibrary}>
+        {catalogingLibrary && catalogingLibrary.data && Object.keys(catalogingLibrary.data).map((field, index)=> {
+          return (<FormGroup label={FluidForm.getLabel(catalogingLibrary, field)} name={field}
+                             className={`${ index === 1 ? 'col-sm-12' :
                             index > 1 && index < 4 ? 'col-sm-3':
                             index === 4 ? 'col-sm-offset-2 col-sm-4':
-                            index > 4 && index < 7 ? 'col-sm-4' : 'col-sm-4'}`}>
-          <input placeholder={FluidForm.getLabel(catalogingLibrary, field)} name={field}
-                 value={FluidForm.getValue(catalogingLibrary,field)}
-                 className="form-control"/>
-        </FormGroup>);
-      })}
-    </FluidForm></CatalogingLibraryPageBody>);
+                            index > 3 && index < 8 ? 'col-sm-4':
+                            index > 10 && index < 13 ? 'col-sm-12':
+                            'col-sm-4'}`}>
+            <input placeholder={FluidForm.getLabel(catalogingLibrary, field)} name={field}
+                   value={FluidForm.getValue(catalogingLibrary,field)}
+                   className="form-control"/>
+          </FormGroup>);
+        })}
+      </FluidForm>
+    </div>
+  </CatalogingLibraryPageBody>);
 };
 
 CatalogingLibraryForm.propTypes = {
